e01983a9bc23ceb2be86bdcdcc6a789d3f06f7a0
Neil McGuigan <nmcguigan@rbauction.com>
2014-04-24 12:36:52 -0700

    commit

--- README.md
+++ README.md
@@ -8,15 +8,23 @@ Examples:
 
     select assert_equals( '+18004685865', normalize_phone_number('1 (800) GOT-JUNK', 'CA') ); --E164 format
 
-    select * from analyze_phone_number('888.867.5309 x666', 'CA');
+    select * from analyze_phone_number('888.867.5309 x666', 'US');
 
-    country_code    national_number     extension   region_code
-    smallint        bigint              text        character(2)
-    1               8888675309          '666'       'US'
+    country_code  national_number  extension  region_code   is_valid_number_for_region  number_type  area_code  local_number
+    smallint      bigint           text       character(2)  boolean                     text         smallint   int
+    1             8888675309       666        US            true                        TOLL_FREE    888        8675309
 
+
+    set vans.default_country_code = 'US';
+    select assert_true( is_phone_number('800Starbuc') );
 
     select assert_false( is_hostname('a-.com') );
 
     select assert_equals( 'www.hotmail.com', normalize_hostname(' WWW.HOTMAIL.COM  ') );
+
+Available settings:
+
+set vans.default_country_code = 'US';  Used for telephone numbers and postal codes
+set vans.allow_local_domains = false;  Used for email addresses and URLs.
 
 More coming soon.
--- vans.sql
+++ vans.sql
@@ -1,4 +1,11 @@
--- you should run load_libphonenumber.sql first.
+/*
+you should run load_libphonenumber.sql first.
+
+available settings:
+set vans.default_country_code = 'US';
+set vans.allow_local_domains = false;
+
+*/
 
 CREATE EXTENSION IF NOT EXISTS plv8;
 
@@ -105,6 +112,7 @@ begin
 
     perform assert_null(normalize_hostname(null));
 
+    perform assert_equals('localhost', normalize_hostname('localhost'));
     perform assert_equals('www.hotmail.com', normalize_hostname(' WWW.HoTMaIl.com  '));
 
 end $body$;
@@ -113,8 +121,23 @@ create or replace function analyze_hostname(text) returns record strict immutabl
      throw 'Not implemented';
 $body$;
 
+set vans.default_country_code = 'CA';
+
+drop type if exists phone_number cascade;
+
+create type phone_number as (
+  country_code smallint,
+  national_number bigint,
+  extension varchar(11),
+  region_code char(2),
+  is_valid_number_for_region boolean,
+  number_type text,
+  area_code smallint, --can be null (malta)
+  local_number int
+);
+
 CREATE OR REPLACE FUNCTION is_phone_number(TEXT, CHAR(2))
-  RETURNS BOOLEAN STRICT IMMUTABLE LANGUAGE plv8 AS $$
+  RETURNS BOOLEAN STRICT IMMUTABLE LANGUAGE plv8 AS $body$
 
 	  if(typeof i18n === 'undefined') {
 		  plv8.find_function('vans.load_libphonenumber')();
@@ -131,7 +154,11 @@ CREATE OR REPLACE FUNCTION is_phone_number(TEXT, CHAR(2))
 		}
 		return false;
 	}
-$$;
+$body$;
+
+create or replace function is_phone_number(text) returns boolean strict volatile language sql as '
+  select vans.is_phone_number($1, current_setting(''vans.default_country_code''));
+';
 
 CREATE OR REPLACE FUNCTION test_is_phone_number()
   RETURNS VOID LANGUAGE plpgsql AS $body$
@@ -144,6 +171,9 @@ BEGIN
     PERFORM assert_true(is_phone_number('1 800 GOT JUNK', 'US'));
 
     perform assert_false(is_phone_number('6045551212','CH'));
+
+    set local vans.default_country_code = 'CA';
+    perform assert_true(is_phone_number('6045551212'));
 
 END $body$;
 
@@ -159,6 +189,10 @@ CREATE OR REPLACE FUNCTION normalize_phone_number(TEXT, CHAR(2))
 		return phoneNumberUtil.format(phoneNumber, i18n.phonenumbers.PhoneNumberFormat.E164);
 
 $body$;
+
+create or replace function normalize_phone_number(text) returns TEXT strict volatile language sql as '
+  select vans.normalize_phone_number($1, current_setting(''vans.default_country_code''));
+';
 
 CREATE OR REPLACE FUNCTION test_normalize_phone_number()
   RETURNS VOID LANGUAGE plpgsql AS $body$
@@ -185,22 +219,32 @@ create or replace function analyze_phone_number(text, char(2)) returns phone_num
 
 $body$;
 
+create or replace function analyze_phone_number(text) returns phone_number strict volatile language sql as '
+  select vans.analyze_phone_number($1, current_setting(''vans.default_country_code''));
+';
+
 CREATE OR REPLACE FUNCTION test_analyze_phone_number()
   RETURNS VOID LANGUAGE plpgsql AS $body$
 BEGIN
 
     PERFORM assert_null(analyze_phone_number(null, null));
 
-    perform assert_equals('(1,7787089999,,"CA")', analyze_phone_number('7787089999', 'CA'));
-    perform assert_equals('(1,7787089999,"123","CA")', analyze_phone_number('7787089999 ext. 123', 'CA'));
-    perform assert_equals('(1,8007827282,,"US")', analyze_phone_number('800-Starbuc', 'US'));
-    perform assert_equals('(1,8888575309,666,"US")', analyze_phone_number('888.857.5309 x666', 'CA'));
+    perform assert_equals('(1,7787089999,,"CA",,,,)', analyze_phone_number('7787089999', 'CA'));
+    perform assert_equals('(1,7787089999,"123","CA",,,,)', analyze_phone_number('7787089999 ext. 123', 'CA'));
+    perform assert_equals('(1,8007827282,,"US",,,,)', analyze_phone_number('800-Starbuc', 'US'));
+    perform assert_equals('(1,8888575309,666,"US",,,,)', analyze_phone_number('888.857.5309 x666', 'CA'));
 
     /* expected exceptions here: */
-    perform assert_equals('(,,,)', analyze_phone_number('not a number', ''));
+    perform assert_equals('(,,,,,,,)', analyze_phone_number('not a number', ''));
     exception when internal_error then return;
 
 END $body$;
+
+drop type if exists domain_name cascade;
+create type domain_name as (
+  public_suffix text,
+  top_private_domain text
+);
 
 create or replace function is_domain_name(text) returns boolean strict immutable language plv8 as $body$
     throw 'Not implemented';
@@ -218,13 +262,34 @@ create or replace function is_postal_code(postal_code text, country_code char(2)
     throw 'Not implemented';
 $body$;
 
+create or replace function is_postal_code(text) returns boolean strict volatile language sql as '
+  select vans.is_postal_code($1, current_setting(''vans.default_country_code''));
+';
+
 create or replace function normalize_postal_code(postal_code text, country_code char(2)) returns text strict immutable language plv8 as $body$
     throw 'Not implemented';
 $body$;
 
+create or replace function normalize_postal_code(text) returns text strict volatile language sql as '
+  select vans.normalize_postal_code($1, current_setting(''vans.default_country_code''));
+';
+
 create or replace function analyze_postal_code(postal_code text, country_code char(2)) returns record strict immutable language plv8 as $body$
     throw 'Not implemented';
 $body$;
+
+create or replace function analyze_postal_code(text) returns record strict volatile language sql as '
+  select vans.analyze_postal_code($1, current_setting(''vans.default_country_code''));
+';
+
+drop type if exists email_address cascade;
+create type email_address as (
+  local_part text,
+  domain_part text,
+  tag text,
+  comments text[],
+  is_webmail boolean
+);
 
 create or replace function is_email_address(text) returns boolean strict immutable language plv8 as $body$
     throw 'Not implemented';
@@ -238,6 +303,19 @@ create or replace function analyze_email_address(text) returns record strict imm
     throw 'Not implemented';
 $body$;
 
+drop type if exists url cascade;
+create type url as (
+  protocol text,
+  host text,
+  host_no_www text,
+  port smallint,
+  defaultPort smallint,
+  file text,
+  path text,
+  query text,
+  anchor text
+);
+
 create or replace function is_url(text) returns boolean strict immutable language plv8 as $body$
     throw 'Not implemented';
 $body$;
@@ -249,6 +327,19 @@ $body$;
 create or replace function analyze_url(text) returns record strict immutable language plv8 as $body$
     throw 'Not implemented';
 $body$;
+
+drop type if exists ip_geo cascade;
+create type ip_geo as (
+  continent_code char(2),
+  country_code char(2),
+  region_code char(5),
+  city_name text,
+  postal_code text,
+  longitude decimal(9,6),
+  latitude decimal(8,6),
+  area_code smallint,
+  metro_code smallint
+);
 
 create or replace function analyze_ip_address(inet) returns record strict immutable language plv8 as $body$
     throw 'Not implemented';
